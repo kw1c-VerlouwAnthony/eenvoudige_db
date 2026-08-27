1d14b2dc682c124b0493217646395dbefc136e49
Anthony Verlouw <anthony.verlouw@edu-kw1c.nl>
2026-08-27 12:29:01 +0200

    dit is mijn eerste commit

--- workshops/index.php
+++ workshops/index.php
@@ -6,3 +6,30 @@
  */
 
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/stylesheet.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>hello world</h1>";
+
+        echo "<p>test</p>";
+
+        //variabele
+        $schoolName = "KW1C";
+
+        echo "mijn school is $schoolName";
+
+        echo 6*6;
+    ?>
+</body>
+</html>
+
